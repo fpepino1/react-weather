de69fab6199a0cd0cdf7f7198902fe6146a6de3f
fpepino1 <115742428+fpepino1@users.noreply.github.com>
2024-03-09 21:01:45 -0500

    API integration

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import "./App.css";
 
+import Weather from "./Weather";
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <Weather defaultCity="New York" />
+        <footer>
+          {" "}
+          This project was coded by Farrah Pepino and is
+          <a href="https://github.com/fpepino1/react-weather">
+            {" "}
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
+    </div>
+  );
 }
 
 export default App;
